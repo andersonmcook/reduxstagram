f1e57557c25a72dc616b4926bc56eac3f3c57f44
andersonmcook <andersonmcook@gmail.com>
2016-06-11 16:47:27 -0500

    hooking up store

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -4,6 +4,6 @@ import { routerReducer } from 'react-router-redux'
 import posts from './posts'
 import comments from './comments'
 
-const rootReducer = combineReducers({posts, comments, router: routerReducer})
+const rootReducer = combineReducers({posts, comments, routing: routerReducer})
 
 export default rootReducer
