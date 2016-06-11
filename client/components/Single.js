8c8a0a72f0916ae25ab7f4b0db09c6e31f6ae3ee
andersonmcook <andersonmcook@gmail.com>
2016-06-11 12:09:14 -0500

    building up components, using router

---
+++ client/components/Single.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Single = React.createClass({
+  render() {
+    return (
+      <div className='single-photo'>
+      single
+      </div>
+    )
+  }
+})
+
+export default Single
--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -6,6 +6,19 @@ import css from './styles/style.styl'
 
 // import components
 import Main from './components/Main'
+import Single from './components/Single'
+import PhotoGrid from './components/PhotoGrid'
 
+// import react router deps
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 
-render(<Main/>, document.getElementById('root'))
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={PhotoGrid}></IndexRoute>
+      <Route path="/view/:postId" component={Single}></Route>
+    </Route>
+  </Router>
+)
+
+render(router, document.getElementById('root'))
