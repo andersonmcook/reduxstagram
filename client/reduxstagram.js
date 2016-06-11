afd95d3de93a2c06aa08b984e1615c8c78b7f519
andersonmcook <andersonmcook@gmail.com>
2016-06-11 11:44:30 -0500

    added components, styles

---
+++ client/reduxstagram.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+// import css
+import css from './styles/style.styl'
+
+// import components
+import Main from './components/Main'
+
+
+render(<Main/>, document.getElementById('root'))
