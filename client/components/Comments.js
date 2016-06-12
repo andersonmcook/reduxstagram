a8a3f7c8ab973ee766537864f10341275c1a57be
andersonmcook <andersonmcook@gmail.com>
2016-06-12 16:02:35 -0500

    comments reducer and subreducer

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -1,13 +1,22 @@
 import React from 'react'
 
 const Comments = React.createClass({
+  handleSubmit(e) {
+    e.preventDefault()
+    const { postId } = this.props.params
+    const author = this.refs.author.value
+    const comment = this.refs.comment.value
+    this.props.addComment(postId, author, comment)
+    this.refs.commentForm.reset()
+  },
+
   renderComment(comment, i) {
     return (
       <div className='comment' key={i}>
         <p>
           <strong>{comment.user}</strong>
           {comment.text}
-          <button className='remove-comment'>&times;</button>
+          <button onClick={this.props.removeComment.bind(null, this.props.params.postId, i)} className='remove-comment'>&times;</button>
         </p>
       </div>
     )
@@ -17,7 +26,7 @@ const Comments = React.createClass({
     return (
       <div className='comments'>
         {this.props.postComments.map(this.renderComment)}
-        <form ref='commentForm' className='comment-form'>
+        <form onSubmit={this.handleSubmit} ref='commentForm' className='comment-form'>
           <input type='text' ref='author' placeholder='author'/>
           <input type='text' ref='comment' placeholder='comment'/>
           <input type='submit' hidden />
