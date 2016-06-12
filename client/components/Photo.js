9319ed5bbbb0f49e10055c975259c9a3ec0ad38f
andersonmcook <andersonmcook@gmail.com>
2016-06-12 14:05:17 -0500

    likes reducer

--- client/components/Photo.js
+++ client/components/Photo.js
@@ -18,7 +18,7 @@ const Photo = React.createClass({
         <figcaption>
           <p>{post.caption}</p>
           <div className='control-buttons'>
-            <button className='likes'>
+            <button onClick={this.props.increment.bind(null, i)} className='likes'>
               &hearts; {post.likes}
             </button>
             <Link className='button' to={`/view/${post.code}`}>
--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -3,9 +3,18 @@
 // 2. copy of current state
 
 function posts(state = [], action) {
-  console.log('the post will change')
-  console.log(state, action)
-  return state
+  switch(action.type) {
+    case 'INCREMENT_LIKES':
+      console.log('incrementing likes')
+      const i = action.index
+      return [
+        ...state.slice(0,i),
+        {...state[i], likes: state[i].likes + 1},
+        ...state.slice(i + 1)
+      ]
+    default:
+      return state
+  }
 }
 
 export default posts
