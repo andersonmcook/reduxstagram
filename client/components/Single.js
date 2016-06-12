abab633ec831f3a8eab7af839356737b29f24939
andersonmcook <andersonmcook@gmail.com>
2016-06-12 14:25:22 -0500

    comments jsx

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,10 +1,19 @@
 import React from 'react'
-
+import Photo from './Photo'
+import Comments from './Comments'
 const Single = React.createClass({
   render() {
+    // index of post
+    // get us the post
+    const { postId } = this.props.params
+    const i = this.props.posts.findIndex(post => post.code === postId)
+    const post = this.props.posts[i]
+    const postComments = this.props.comments[postId] || []
+
     return (
       <div className='single-photo'>
-      single
+        <Photo i={i} post={post} {...this.props}/>
+        <Comments postComments={postComments}/>
       </div>
     )
   }
